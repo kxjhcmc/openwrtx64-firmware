71f48b757da8863db649f2315c861963984cf87f
kxjhcmc <32127921+kxjhcmc@users.noreply.github.com>
2022-07-27 11:38:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,8 @@
 #svn co https://github.com/Ljzkirito/openwrt-packages/trunk/ppp package/network/services/ppp
 #svn co https://github.com/openwrt/openwrt/trunk/package/network/services/ppp package/network/services/ppp
 # Add luci-theme-argon
-rm -rf package/lean/luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
+#rm -rf package/lean/luci-theme-argon
+#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
 #Add luci-app-passwall
 #svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/lean/luci-app-passwall
